11cd11b291443652c78c55ff2535ae5512b55e38
Jamey J. DeOrio <jdeorio@safaribooksonline.com>
2015-10-28 18:16:16 -0700

    Open the correct envdir

--- reallylonglink/wsgi.py
+++ reallylonglink/wsgi.py
@@ -16,6 +16,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reallylonglink.settings")
 
-envdir.open('/home/jameydeorio/')
+envdir.open('/home/jameydeorio/webapps/reallylonglink/envdir')
 
 application = get_wsgi_application()
